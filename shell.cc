272c1abeff853dd2521193bc90f4ebeb5f99cd58
Daizong Wu <wu1418@data.cs.purdue.edu>
2020-07-16 15:04:49 -0400

    \Part 1 Total:            20.0 of 20.0\

--- shell.cc
+++ shell.cc
@@ -11,8 +11,37 @@ void Shell::prompt() {
   }
 }
 
+void sigIntHandler (int sig) {
+  
+}
+
+void sigChildHandler (int sig) {
+
+  printf("%d exited.\n", pid);
+}
+
 int main() {
   Shell::prompt();
+
+  char s[20];
+  fflush(stdout);
+  fgets(s, 20, stdin);
+  if (!strcmp(s, "exit\n")) {
+    printf("Exiting...\n");
+    exit(1);
+  }
+  
+  struct sigaction signalAction;
+  signalAction.sa_handler = sigIntHandler;
+  sigemptyset(&signalAction.sa_mask);
+  signalAction.sa_flags = SA_RESTART;
+
+  int error = sigaction(SIGINT, &signalAction, NULL);
+  if (error) {
+    perror("sigaction");
+    exit(-1);
+  }
+  
   yyparse();
 }
 

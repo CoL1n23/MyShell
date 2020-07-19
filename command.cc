64e8db83dd99dffcf83a4bc25520717dc7e11ff8
Daizong Wu <wu1418@data.cs.purdue.edu>
2020-07-18 23:50:25 -0400

    \Part 2 Total:            6.0 of 22.0\

--- command.cc
+++ command.cc
@@ -119,7 +119,7 @@ void Command::setenv(int i) {
   strcpy(arg1, _simpleCommands[i]->_arguments[1]->c_str());
   char* arg2 = new char[_simpleCommands[i]->_arguments[2]->length() + 1]; 
   strcpy(arg2, _simpleCommands[i]->_arguments[2]->c_str());
-  fprintf(stderr, "%s\n", arg1);
+  fprintf(stderr, "%d\n", strlen(arg1));
   //if (putenv(result.c_str()) {
   //  perror("putenv");
   //  exit(1);

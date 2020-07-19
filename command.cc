c88c5f35497cab233b77f46d188d79170078eb88
Daizong Wu <wu1418@data.cs.purdue.edu>
2020-07-18 23:55:56 -0400

    \Part 2 Total:            6.0 of 22.0\

--- command.cc
+++ command.cc
@@ -119,7 +119,8 @@ void Command::setenv(int i) {
   strcpy(arg1, _simpleCommands[i]->_arguments[1]->c_str());
   char* arg2 = new char[_simpleCommands[i]->_arguments[2]->length() + 1]; 
   strcpy(arg2, _simpleCommands[i]->_arguments[2]->c_str());
-  char* equal = "=\0";
+  char* equal = new char[2];
+  equal = "=\0";
   char* arg = new char[strlen(arg1) + strlen(arg2) + 2];
   strcpy(arg, arg1);
   strcat(arg, equal);

f476238116cb3f984620e8c2e4bc3dc98ec7fdd4
Daizong Wu <wu1418@data.cs.purdue.edu>
2020-07-19 00:48:02 -0400

    \Part 2 Total:            6.5 of 22.0\

--- command.cc
+++ command.cc
@@ -134,15 +134,16 @@ void Command::setenv(int i) {
 void Command::unsetenv(int i) {
   extern char** environ;
   char** p = environ;
-  while (*p != NULL) {
+  int index = 0;
+  while (p[index] != NULL) {
     char* name;
     const char s[2] = "=";
-    name = strtok(*p, s);
+    name = strtok(p[index], s);
     printf("%s\n", name);
     //if (!strcmp(_simpleCommands[i]->_arguments[1]->c_str(), name)) {
       
     //}
-    p++;
+    index++;
   }
   exit(0);
 }

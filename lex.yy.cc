8e0173cba240c72d3e54ee250ccd5c22466b4652
Daizong Wu <wu1418@data.cs.purdue.edu>
2020-07-19 20:55:55 -0400

    \Part 3 Total:            1.0 of 28.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -988,7 +988,7 @@ YY_RULE_SETUP
     read(0, result1, 50);
     fprintf(stderr, "child: %s\n", result1);
     fprintf(stderr, "%d\n", strlen(result1));
-    std::string arg = new std::string(result1);
+    std::string arg(result1);
     fprintf(stderr, "%d\n", arg.size());
  
     close(pin[0]);

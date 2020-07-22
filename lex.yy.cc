c630ad6c75c66cede1d84b110946d1a693292044
Daizong Wu <wu1418@data.cs.purdue.edu>
2020-07-22 13:11:46 -0400

    \Part 1 Total:            20.0 of 20.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1092,9 +1092,14 @@ YY_RULE_SETUP
     if (char_count != -1) {
       // unput line in source file
       printf("%s %d\n", line, strlen(line));
-      for (size_t i = strlen(line) - 1; i >= 0; i--) {
-        printf("%c\n", line[i]);
-        myunputc(line[i]);
+      char* linec = new char[strlen(line) + 1];
+      for (int i = 0; i < strlen(line); i++) {
+        linec[i] = line[i];
+      }
+      linec[strlen(line)] = '\0';
+      for (size_t i = strlen(linec) - 1; i >= 0; i--) {
+        printf("%c\n", linec[i]);
+        myunputc(linec[i]);
       }
       myunputc('\n');
     }
@@ -1109,7 +1114,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 230 "shell.l"
+#line 235 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -1118,10 +1123,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 235 "shell.l"
+#line 240 "shell.l"
 ECHO;
 	YY_BREAK
-#line 1125 "lex.yy.cc"
+#line 1130 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -2138,4 +2143,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 235 "shell.l"
+#line 240 "shell.l"

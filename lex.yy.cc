75dc8a21b50c720b880c141060c2ff7a92a4a589
Daizong Wu <wu1418@data.cs.purdue.edu>
2020-07-22 11:16:12 -0400

    \Part 1 Total:            20.0 of 20.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1071,12 +1071,20 @@ case 14:
 YY_RULE_SETUP
 #line 193 "shell.l"
 {
+  // handle source file
   printf("%s\n", yytext);
+  char* filename = new char[strlen(yytext) - 6];
+  filename[strlen(filename) - 1] = '\0';
+  int index = 0;
+  for (int i = 7; i < strlen(yytext); i++) {
+    filename[index++] = yytext[i];
+  }
+  printf("%s\n", filename);
 }
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 197 "shell.l"
+#line 205 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -1085,10 +1093,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 202 "shell.l"
+#line 210 "shell.l"
 ECHO;
 	YY_BREAK
-#line 1092 "lex.yy.cc"
+#line 1100 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -2105,4 +2113,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 202 "shell.l"
+#line 210 "shell.l"

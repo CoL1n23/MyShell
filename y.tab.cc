a49f2880c69e736821d034133f69b167efba5bfb
Daizong Wu <wu1418@data.cs.purdue.edu>
2020-07-24 18:08:51 -0400

    \Part 2 Total:            22.0 of 22.0\

--- y.tab.cc
+++ y.tab.cc
@@ -178,8 +178,8 @@ void yyerror(const char * s);
 int yylex();
 
 int compareFiles(const void* file1, const void* file2) {
-  char* cp1 = strdup((char *)file1);
-  char* cp2 = strdup((char *)file2);
+  char* cp1 = strdup((const char *)file1);
+  char* cp2 = strdup((const char *)file2);
   
   int index = 0;    
   while (cp1[index] != '\0') {
